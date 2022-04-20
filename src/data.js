105994dfa79ef6829a6def8988c197b8a152b96a
Akari Amir <amir.akari@esprit.tn>
2022-04-20 22:19:35 +0200

    Product lists work correctly :tada:

---
+++ src/data.js
@@ -0,0 +1,115 @@
+export const storeProducts = [
+    {
+        id: 1,
+        title:"BENCO Y30 - Blue",
+        img:"img/Benco.jpg",
+        price:229,
+        company:"Benco",
+        info:"Double SIM - Ecran 5.71\" HD IPS - Processeur Unisoc SC7731e, Quad-core, 4×ARM Cortex-A7, Mali-T820 - RAM 3 Go - Mémoire 16 Go Extensible jusqu'à 128 Go - Android 9.0 Pie (Go edition) - Appareils Photo 5MP Arrière, 5MP Frontal - 3G - Wifi - Bluetooth - Lecteur d'empreintes digitales - Batterie 2500 mAh - Couleur Bleu - Garantie 1 an",
+        inCart: false,
+        count:0,
+        total:0
+    },
+    {
+        id: 2,
+        title:"NOKIA C1 2EME EDITION - Black",
+        img:"img/nokia.jpg",
+        price:240,
+        company:"NOKIA",
+        info:"Double SIM - Ecran 5.45\" FWVGA+ IPS - Processeur Quad Core 1.3 GHz - RAM 1 Go - Mémoire 16 Go Extensible jusqu'à 64 Go - Android 9.0 Pie - Appareils Photo: 5MP Arrière, 5MP Frontal - 3G - Wifi - Bluetooth - GPS - Batterie 2500 mAh - Couleur Bleu - Garantie 1 an",
+        inCart: false,
+        count:0,
+        total:0
+    },
+    {
+        id: 3,
+        title:"HUAWEI Y5P 2020 - Black",
+        img:"img/huawei.jpg",
+        price:369,
+        company:"HUAWEI",
+        info:"Double SIM - Ecran 5.45\" IPS LCD - Résolution d'écran: (720 x 1440 px) - Processeur Mediatek MT6762R, octa-core, 4 x cœurs Cortex-A53 2.0 GHz + 4 x cœurs Cortex-A53 1.5 GHz - Android 10 - RAM 2 Go - Mémoire 32 Go - Caméra frontale 5 MP - Caméra arrière 8 MP - Wifi - 4G - GPS - Bluetooth - Batterie 3020 mAh - Couleur: Noir - Garantie 1 an + Écouteurs Huawei AM115 Offerts",
+        inCart: false,
+        count:0,
+        total:0
+    },
+    {
+        id: 4,
+        title:"ZTE BLADE A51 - Gris",
+        img:"img/zte.jpg",
+        price:309,
+        company:"ZTE",
+        info:"Double SIM - Ecran 6.52\" HD+ 720 x 1600 pixels - Processeur Unisoc Octa Core (4 x 1.6 GHz + 4 x 1.2 GHz) - RAM 2 Go - Mémoire 32 Go - Android 11 Go - Double caméra arrière: 13MP + 2MP / Caméra frontale 5MP - Lecteur d'empreinte digitale - 4G - Bluetooth 4.2 - Wifi - Batterie 3200 mAh - Couleur Gris - Garantie 1 an + Coque silicone offerte ",
+        inCart: false,
+        count:0,
+        total:0
+    },
+    {
+        id: 5,
+        title:"REALME C21Y - Black",
+        img:"img/realme.jpg",
+        price:489,
+        company:"realme",
+        info:"Double SIM - Ecran 6.5\" LCD 720 x 1600 pixels - Processeur Unisoc T610 Octa-core (2x1.8 GHz Cortex-A75 & 6x1.8 GHz Cortex-A55), Mali-G52 - RAM 4 Go - Mémoire 64 Go - Android 11, Realme UI - Appareil Photo IA 13 MP + 2 MP + 2 MP, HDR (Arrière), IA 5.0 MP, HDR (Frontale) - Lecteur d'empreinte digitale montée à l'arrière - 4G - Wifi - Bluetooth 5.0 - Batterie 5000 mAh - Couleur Noir - Garantie 1 an",
+        inCart: false,
+        count:0,
+        total:0
+    }
+]
+export const detailProduct =
+    [
+        {
+            id: 1,
+            title:"BENCO Y30 - Blue",
+            img:"img/Benco.jpg",
+            price:229,
+            company:"Benco",
+            info:"Double SIM - Ecran 5.71\" HD IPS - Processeur Unisoc SC7731e, Quad-core, 4×ARM Cortex-A7, Mali-T820 - RAM 3 Go - Mémoire 16 Go Extensible jusqu'à 128 Go - Android 9.0 Pie (Go edition) - Appareils Photo 5MP Arrière, 5MP Frontal - 3G - Wifi - Bluetooth - Lecteur d'empreintes digitales - Batterie 2500 mAh - Couleur Bleu - Garantie 1 an",
+            inCart: true,
+            count:0,
+            total:0
+        },
+        {
+            id: 2,
+            title:"NOKIA C1 2EME EDITION - Black",
+            img:"img/nokia.jpg",
+            price:240,
+            company:"NOKIA",
+            info:"Double SIM - Ecran 5.45\" FWVGA+ IPS - Processeur Quad Core 1.3 GHz - RAM 1 Go - Mémoire 16 Go Extensible jusqu'à 64 Go - Android 9.0 Pie - Appareils Photo: 5MP Arrière, 5MP Frontal - 3G - Wifi - Bluetooth - GPS - Batterie 2500 mAh - Couleur Bleu - Garantie 1 an",
+            inCart: false,
+            count:0,
+            total:0
+        },
+        {
+            id: 3,
+            title:"HUAWEI Y5P 2020 - Black",
+            img:"img/huawei.jpg",
+            price:369,
+            company:"HUAWEI",
+            info:"Double SIM - Ecran 5.45\" IPS LCD - Résolution d'écran: (720 x 1440 px) - Processeur Mediatek MT6762R, octa-core, 4 x cœurs Cortex-A53 2.0 GHz + 4 x cœurs Cortex-A53 1.5 GHz - Android 10 - RAM 2 Go - Mémoire 32 Go - Caméra frontale 5 MP - Caméra arrière 8 MP - Wifi - 4G - GPS - Bluetooth - Batterie 3020 mAh - Couleur: Noir - Garantie 1 an + Écouteurs Huawei AM115 Offerts",
+            inCart: false,
+            count:0,
+            total:0
+        },
+        {
+            id: 4,
+            title:"ZTE BLADE A51 - Gris",
+            img:"img/zte.jpg",
+            price:309,
+            company:"ZTE",
+            info:"Double SIM - Ecran 6.52\" HD+ 720 x 1600 pixels - Processeur Unisoc Octa Core (4 x 1.6 GHz + 4 x 1.2 GHz) - RAM 2 Go - Mémoire 32 Go - Android 11 Go - Double caméra arrière: 13MP + 2MP / Caméra frontale 5MP - Lecteur d'empreinte digitale - 4G - Bluetooth 4.2 - Wifi - Batterie 3200 mAh - Couleur Gris - Garantie 1 an + Coque silicone offerte ",
+            inCart: false,
+            count:0,
+            total:0
+        },
+        {
+            id: 5,
+            title:"REALME C21Y - Black",
+            img:"img/realme.jpg",
+            price:489,
+            company:"realme",
+            info:"Double SIM - Ecran 6.5\" LCD 720 x 1600 pixels - Processeur Unisoc T610 Octa-core (2x1.8 GHz Cortex-A75 & 6x1.8 GHz Cortex-A55), Mali-G52 - RAM 4 Go - Mémoire 64 Go - Android 11, Realme UI - Appareil Photo IA 13 MP + 2 MP + 2 MP, HDR (Arrière), IA 5.0 MP, HDR (Frontale) - Lecteur d'empreinte digitale montée à l'arrière - 4G - Wifi - Bluetooth 5.0 - Batterie 5000 mAh - Couleur Noir - Garantie 1 an",
+            inCart: false,
+            count:0,
+            total:0
+        }
+    ]
--- src/index.js
+++ src/index.js
@@ -4,13 +4,15 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import {BrowserRouter as Router} from "react-router-dom";
+import {ProductProvider} from "./context";
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+      <ProductProvider>
       <Router>
           <App />
       </Router>
-
+          </ProductProvider>
   </React.StrictMode>
 );
 
